1fd2c0f1e27595b560fdd3e53b14aac64bb1622c
Erich Menge <erich.menge@me.com>
2013-03-24 14:26:22 -0500

    Get rid of unnecessary nest.

--- lib/signed_form/hmac.rb
+++ lib/signed_form/hmac.rb
@@ -2,35 +2,35 @@ require 'openssl'
 
 module SignedForm
   module HMAC
-    class << self
-      attr_accessor :secret_key
+    extend self
 
-      def create_hmac(data)
-        if secret_key.nil? || secret_key.empty?
-          raise Errors::NoSecretKey, "Please consult the README for instructions on creating a secret key"
-        end
+    attr_accessor :secret_key
 
-        OpenSSL::HMAC.hexdigest OpenSSL::Digest::SHA1.new, secret_key, data
+    def create_hmac(data)
+      if secret_key.nil? || secret_key.empty?
+        raise Errors::NoSecretKey, "Please consult the README for instructions on creating a secret key"
       end
 
-      def verify_hmac(signature, data)
-        if secret_key.nil? || secret_key.empty?
-          raise Errors::NoSecretKey, "Please consult the README for instructions on creating a secret key"
-        end
+      OpenSSL::HMAC.hexdigest OpenSSL::Digest::SHA1.new, secret_key, data
+    end
 
-        secure_compare OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret_key, data), signature
+    def verify_hmac(signature, data)
+      if secret_key.nil? || secret_key.empty?
+        raise Errors::NoSecretKey, "Please consult the README for instructions on creating a secret key"
       end
 
-      # After the Rack implementation
-      def secure_compare(a, b)
-        return false unless a.bytesize == b.bytesize
+      secure_compare OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA1.new, secret_key, data), signature
+    end
 
-        l = a.unpack("C*")
+    # After the Rack implementation
+    def secure_compare(a, b)
+      return false unless a.bytesize == b.bytesize
 
-        r, i = 0, -1
-        b.each_byte { |v| r |= v ^ l[i+=1] }
-        r == 0
-      end
+      l = a.unpack("C*")
+
+      r, i = 0, -1
+      b.each_byte { |v| r |= v ^ l[i+=1] }
+      r == 0
     end
   end
 end
