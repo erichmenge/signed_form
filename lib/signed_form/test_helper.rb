e9f695b49edc16a42b07240911663cbb64d7def4
Erich Menge <erich.menge@me.com>
2013-04-30 22:54:24 -0500

    No need to create a module, the singleton suffices.

--- lib/signed_form/test_helper.rb
+++ lib/signed_form/test_helper.rb
@@ -1,17 +1,15 @@
 module SignedForm
   module TestHelper
     def permit_all_parameters
-      mod = Module.new do
+      @controller.singleton_class.class_eval do
         def params
           super.dup.permit!
         end
       end
 
-      @controller.singleton_class.send :include, mod
-
       if block_given?
         yield
-        mod.send :remove_method, :params
+        @controller.singleton_class.send :remove_method, :params
       end
     end
   end
